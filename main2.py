19c7c35fbcd4b4ff2d458350d6aa16d1be531c0f
BoxiGod <DevirLand@gmail.com>
2020-11-25 22:17:32 +0300

    add second task

---
+++ main2.py
@@ -0,0 +1,18 @@
+import hashlib
+
+
+def md5_hash(some_file):
+    i = 0
+    lines = some_file.readlines()
+    while i < len(lines):
+        yield hashlib.md5(lines[i].encode())
+        i += 1
+
+
+if __name__ == '__main__':
+    some_f = open('countries.json', 'r')
+    for line in md5_hash(some_f):
+        print(line.hexdigest())
+
+
+
